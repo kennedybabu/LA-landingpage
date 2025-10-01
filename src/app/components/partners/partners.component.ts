3b6847a7ee1265a9b79401793927f62ce0516770
kennedybabu <kennedynjogu8@gmail.com>
2025-10-01 11:27:00 +0300

    feature: add products carousel

--- src/app/components/partners/partners.component.ts
+++ src/app/components/partners/partners.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { Carousel } from 'primeng/carousel'
+
 
 @Component({
   selector: 'app-partners',
-  imports: [],
+  imports: [Carousel],
   templateUrl: './partners.component.html',
   styleUrl: './partners.component.scss'
 })
 export class PartnersComponent {
+  icons: any[] = [
+    {
+      "title": 1
+    },
+     {
+      "title": 2
+    },
+     {
+      "title": 3
+    },
+     {
+      "title": 4
+    },
+     {
+      "title": 5
+    },
+     {
+      "title": 6
+    }
+  ]
 
 }
